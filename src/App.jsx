ac2e842e3204463e9fa0b35d0a9a60e9f1f69611
GeovannaSCunha2 <geovanna.scunha@gmail.com>
2023-11-05 19:53:57 -0300

    adicionado a estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Cabecalho from './components/Cabecalho'
+import Rodape from './components/Rodape'
+import Home from './components/Home'
 
 
 function App() {
 
   return (
     <>
+    <Cabecalho/>
+    <Home/>
+    <Rodape/>
 
     </>
   )
